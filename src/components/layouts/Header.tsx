b02ad829b8e641c4093828eb70692db19e510b59
RahimliZakir <zakirer@code.edu.az>
2024-03-16 05:00:58 +0400

    Header Done

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <nav className="cont shadow-default h-[64px] flex justify-end">
+        <div className="flex items-center">
+          <p>AZ</p>
+          <div className="ms-4 bg-purple rounded-full w-[32px] h-[32px]"></div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      purple: "#456cd9",
+    },
+    boxShadow: {
+      default: "0px 3px 10px 1px #C7D1DC5C, 0px 1px 3px 0px #C7D1DC40",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
