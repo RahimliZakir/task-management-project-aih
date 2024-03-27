a2a1d54931fd4f4cba10deaa59fb50abfeb7d878
RahimliZakir <zakirer@code.edu.az>
2024-03-27 06:34:33 +0400

    Some Modifications #2

--- src/components/pages/SelectedTask/Index.tsx
+++ src/components/pages/SelectedTask/Index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 
 import Badge from "../../shared/Badge/Index";
 import { SelectOptionValues } from "../../../types/enums/SelectedTask";
