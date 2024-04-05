696e07ee30a928e7fa4ce434ccc349bcac017c39
RahimliZakir <zakirer@code.edu.az>
2024-04-05 13:26:08 +0400

    Logo Added to Navbar

--- src/components/layouts/Header/Index.tsx
+++ src/components/layouts/Header/Index.tsx
@@ -2,6 +2,8 @@ import { CSSObjectWithLabel } from "react-select";
 
 import ReactSelect from "../../shared/ReactSelect/Index";
 
+import logo from "../../../assets/imgs/logo.png";
+
 const Header = () => {
   const reactSelectOptions = [
     { value: "az", label: "AZ" },
@@ -25,7 +27,13 @@ const Header = () => {
     <header>
       <nav className="shadow-default bg-white">
         <div className="cont">
-          <div className="h-[64px] row justify-end">
+          <div className="h-[64px] row justify-between items-center">
+            <div className="w-1/4">
+              <div className="w-[45px]">
+                <img className="w-full" src={logo} alt="AİH Logo" />
+              </div>
+            </div>
+
             <div className="flex items-center">
               <ReactSelect
                 defaultValue={reactSelectOptions[0]}
